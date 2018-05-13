6eac958f6549cef6dc60d56c1216d7fd880cb408
DividedByCero <isaac4893@outlook.com>
2018-05-12 23:59:26 -0300

    reconfigure deploy logic steps

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ let babel = require("gulp-babel");
 let clean = require("gulp-clean");
 
 gulp.task("build:clean", function(){
-    return gulp.src("./build").pipe(clean({force : true}));
+    return gulp.src("./build/**/*.*").pipe(clean({force : true}));
 })
 
 gulp.task("transpile:providers", ["build:clean"], function(){
@@ -14,21 +14,29 @@ gulp.task("transpile:providers", ["build:clean"], function(){
                .pipe(gulp.dest("./build/providers/"))
 });
 
-gulp.task("copy:config", ["transpile:providers"], function(){
-    return gulp.src("./config/**/*")
+gulp.task("transpile:config", ["transpile:providers"], function(){
+    return gulp.src("./config/**/*.js")
+               .pipe(babel({
+                   presets : ["@babel/env"],
+                }))
                .pipe(gulp.dest("./build/config/"))
 });
 
-gulp.task("copy:parent", ["copy:config"], function(){
+gulp.task("copy:parent", ["transpile:config"], function(){
     return gulp.src(["./*", "!./gulpfile.js", "!./node_modules", "!./build"])
                .pipe(gulp.dest("./build/"))
 });
 
-gulp.task("transpile:parent", ["copy:parent"], function(){
+gulp.task("copy:pjFile", ["copy:parent"], function(){
+    return gulp.src("./testDir/*.csproj")
+               .pipe(gulp.dest("./build/"))
+});
+
+gulp.task("transpile:parent", ["copy:pjFile"], function(){
     let sources = [
         "./*.js",
         "!./gulpfile.js"
-    ];   
+    ];
     return gulp.src(sources)
                 .pipe(babel({
                     presets : ["@babel/env"],
