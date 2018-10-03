761b14cf676fd07d48fb274d635c6e1f0da0f734
DividedByCero <isaac4893@outlook.com>
2018-10-03 19:02:51 -0400

    set debugger launch.json config file

--- entry.ts
+++ entry.ts
@@ -2,5 +2,7 @@ import * as fs from "fs-extra";
 import process from "process";
 import { BaseGeneratorArgument } from './types/baseGeneratorArgument';
 import BaseGeneratorFactory from './factories/baseGeneratorFactory';
+import { ObjectTypes } from './constants';
 
-var appArguments : BaseGeneratorArgument;
+var appArguments : BaseGeneratorArgument = BaseGeneratorFactory(process.argv);
+
